9f4eccf1d7a4c503337f793ec3058264074b55d2
sirakisaias <sirakisaias@gmail.com>
2021-11-29 19:22:59 -0800

    finished intro and portfolio page

--- src/component/intro/Intro.js
+++ src/component/intro/Intro.js
@@ -1,14 +1,37 @@
 import "./intro.scss"
+import {KeyboardArrowDown} from "@material-ui/icons"
+import { init } from 'ityped'
+import { useEffect, useRef } from "react"
 
-export default function intro() {
+
+export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, { 
+            showCursor: true, 
+            backDelay: 1500,
+            strings: ['FrontEnd', 'BackEnd', 'UX/UI' ] })
+    }, [])
     return (
         <div className="intro" id="intro">
             <div className="left">
                 <div className="imgContainer">
-                    <img src="assets/myimg.jpg" alt="profile pic"/>
+                    <img src="assets/profile.jpg" alt="profile pic"/>
                 </div>
             </div>
-            <div className="right"></div>    
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Sirak Hailemichael</h1>
+                    <h3><span ref={textRef}></span> Developer</h3>
+                    <a href="#portfolio">
+                        <KeyboardArrowDown className="keyboardArrowDown"/>
+                    </a> 
+                </div>
+                
+            </div>    
         </div>
     )
 }
--- src/component/portfolio/Portfolio.js
+++ src/component/portfolio/Portfolio.js
@@ -1,9 +1,67 @@
+import { useState } from "react"
+import PortfolioList from "../portfolioList/PortfolioList"
 import "./portfolio.scss"
 
 export default function Portfolio() {
+
+    const [selected, setSelected] = useState("featured")
+
+    const list =[
+        {
+            id:"featured",
+            title: "Featured",
+        },
+        {
+            id:"web",
+            title: "Web App",
+        },
+        {
+            id:"design",
+            title: "Design",
+        },
+        {
+            id:"brand",
+            title: "Branding",
+        },
+    ]
     return (
         <div className="portfolio" id="portfolio">
-            
+            <h1>Portfolio</h1>
+            <ul>
+                {list.map(item=>(
+                    <PortfolioList title={item.title}
+                    active={selected === item.id}
+                    setSelected={setSelected}
+                    id = {item.id}
+                    />
+                ))}
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="assets/profile.jpg" alt="profile "/>
+                    <h3>First App</h3>
+                </div>
+                <div className="item">
+                    <img src="https://www.moveoapps.com/blog/wp-content/uploads/2020/07/types-of-web-applications.png" alt=""/>
+                    <h3>Second App</h3>
+                </div>
+                <div className="item">
+                    <img src="https://www.moveoapps.com/blog/wp-content/uploads/2020/07/types-of-web-applications.png" alt=""/>
+                    <h3>Third App</h3>
+                </div>
+                <div className="item">
+                    <img src="https://www.moveoapps.com/blog/wp-content/uploads/2020/07/types-of-web-applications.png" alt=""/>
+                    <h3>Fourth App</h3>
+                </div>
+                <div className="item">
+                    <img src="https://www.moveoapps.com/blog/wp-content/uploads/2020/07/types-of-web-applications.png" alt=""/>
+                    <h3>Fifth App</h3>
+                </div>
+                <div className="item">
+                    <img src="https://www.moveoapps.com/blog/wp-content/uploads/2020/07/types-of-web-applications.png" alt=""/>
+                    <h3>Sixth App</h3>
+                </div>
+            </div>
         </div>
     )
 }
